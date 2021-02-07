c8147a35c15916fe91570a747c2395c11644b0fb
9bini <gotn0727@gmail.com>
2021-02-07 16:18:59 +0900

    ~ Stream API

--- src/com/koobin/stream/StreamApi.java
+++ src/com/koobin/stream/StreamApi.java
@@ -1,56 +1,84 @@
 package com.koobin.stream;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamApi {
     public static void main(String[] args) {
-        List<String> names = new ArrayList<>();
-        names.add("9");
-        names.add("bin");
-        names.add("foo");
-        names.add("bar");
+        List<OnlineClass> springClasses = new ArrayList<>();
+        springClasses.add(new OnlineClass(1, "spring boot", true));
+        springClasses.add(new OnlineClass(2, "spring data jpa", true));
+        springClasses.add(new OnlineClass(3, "spring mvc", false));
+        springClasses.add(new OnlineClass(4, "spring core", false));
+        springClasses.add(new OnlineClass(5, "rest api development", false));
 
-        Stream<String> stringStream = names.stream().map(String::toUpperCase);
-        Stream<String> stringStream2= names.stream().map(s -> {
-            // 출력되지 않는다.
-            // 터미널 오퍼레이터가 오지 않는 이상 실행되지 않는다.
-            // 정의만 해놓은 상태이다. 실행을 하지 않느다. 종료형이 없다면 무의미한 코드다.
-            // 중계 오퍼레이터 = 스트림 파이트라인을 정의해야한다.
-            System.out.println("s = " + s);
-            return s.toUpperCase();
-        });
-        System.out.println("===========");
-        List<String> collect = names.stream().map(s -> {
-            // 종료형이 오기 전까지 통작을 하지않는 느낌을 Lazy하다고 의미로 이해하면된다.
+        System.out.println("spring 으로 시작하는 수업");
+        // 파이프라인을 정의한다.
+        // 특정한 타입이 파이프라인에 지나간다.
+        springClasses.stream()
+                .filter(oc -> oc.getTitle().startsWith("spring"))
+                .forEach(oc-> System.out.println(oc.getId()));
+        System.out.println("----------------");
 
-            System.out.println("s = " + s);
-            return s.toUpperCase();
-        }).collect(Collectors.toList());
+        System.out.println("close 되지 않은 수업");
+        springClasses.stream()
+                .filter(Predicate.not(OnlineClass::isClosed))
+                //.filter(oc -> oc.isClosed())
+                .forEach(oc-> System.out.println(oc.getId()));
+        System.out.println("----------------");
 
-        System.out.println("===========");
+        System.out.println("수업 이름만 모아서 스트림 만들기");
+        springClasses.stream()
+                //.map(oc-> oc.getTitle())
+                .map(OnlineClass::getTitle)
+                .forEach(System.out::println);
+        System.out.println("----------------");
+
+        List<OnlineClass> javaClasses = new ArrayList<>();
+        javaClasses.add(new OnlineClass(6, "The Java, Test", true));
+        javaClasses.add(new OnlineClass(7, "The Java Code manipulation", true));
+        javaClasses.add(new OnlineClass(8, "The Java 8 to 11", false));
+
+
+        List<List<OnlineClass>> binEvents = new ArrayList<>();
+        binEvents.add(springClasses);
+        binEvents.add(javaClasses);
+
+
+        System.out.println("두 수업 목록에 들어있는 모든 아이디 출력");
+        binEvents.stream()
+                //.flatMap(list -> list.stream())
+                .flatMap(Collection::stream)
+                .forEach(oc-> System.out.println(oc.getId()));
+        System.out.println("----------------");
+
+        System.out.println("10 부터 1씩 증가하는 무제한 스트림 주에서 앞에 10개 빼고 최대 10개 까지만");
+        Stream.iterate(10,i->i+1)
+                .skip(10)
+                .limit(10)
+                .forEach(System.out::println);
+        System.out.println("----------------");
+
+        System.out.println("자바 수업 중에 Test가 들어있는 수업이 있는지 확인");
+        boolean test = javaClasses.stream()
+                .anyMatch(oc -> oc.getTitle().contains("Test"));
+        System.out.println("test = " + test);
+        System.out.println("----------------");
+
+        System.out.println("스프링 수업 중에 제목이 spring이 들어간 제목만 모아서 List로 만들기");
+        List<String> collect = springClasses.stream()
+                // map 반환되는 타입을 고려 해야한다.
+                //.map(oc->oc.getTitle())
+                //.filter(title -> title.contains("spring"))
+                .filter(oc -> oc.getTitle().contains("spring"))
+                .map(oc->oc.getTitle())
+                .collect(Collectors.toList());
+
         collect.forEach(System.out::println);
-
-        System.out.println("===========");
-        names.forEach(System.out::println);
-        for (String name: names){
-            if (name.startsWith("9")){
-                System.out.println("name.toUpperCase() = " + name.toUpperCase());
-            }
-        }
-        List<String> collect1 = names.parallelStream().map(String::toUpperCase).collect(Collectors.toList());
-        collect1.forEach(System.out::println);
-        // 주의해야하는 것은 상황에 맞게 사용해야한다. 잘못 사용하게 되면 오히려 성능이 낮아 질 수 있다.
-        // 대부분의 경우는
-        // 데이터가 정말 방대하게 큰 경우, 데이터 소스, 처리하는 내용, 등등 따라서 사용해야하는데
-        // 이런 상황에 맞게 사용하기 힘들기 때문에 실제한번 성능 체크를 해보고 하는 결정하는 것이 현명하다.
-        List<String> collect2 = names.parallelStream().map((s -> {
-            System.out.println("s = " + s + Thread.currentThread().getName());
-            return s.toUpperCase();
-                })
-        ).collect(Collectors.toList());
-
     }
+
 }
