e370cace3e0c0e79efa0e0659ca4952189ee3af7
9bini <gotn0727@gmail.com>
2021-02-27 17:41:08 +0900

    Executors

--- src/com/koobin/thread/App.java
+++ src/com/koobin/thread/App.java
@@ -1,76 +1,42 @@
 package com.koobin.thread;
 
+import java.util.concurrent.*;
+
 public class App {
     public static void main(String[] args){
-        // 지금 동작하는 쓰레드 main 쓰레드이다
-        System.out.println("hello: " + Thread.currentThread().getName());
-        // 동작 순서는 보장 할 수 없다.
+        Executor executor;
+        ExecutorService executorService;
+        ScheduledExecutorService scheduledExecutorService;
 
-        // 메인 쓰레드에서 다른 쓰레드를 만들 때 방법
-        // 1. 쓰레드 상속
-        MyThread myThread = new MyThread();
-        myThread.start();
-        // 2.쓰레드 생성자에게 Runnable 주는 것
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Thread" + Thread.currentThread().getName());
-            }
-        });
-        thread.start();
+        executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(getRunnable("Thread"));
+        // 주의할 것 실행이 되지만 executorService 만들어서 어떤 작업을 실핼시키면 다음 작업이 올때까지 계속해서 대기한다.
+        // 그래서 프로세스가 죽지않는다. 계속 켜져있다.
+        // 명시적을 shutdown을 해야한다.
 
-        // 쓰레드의 주요 기능 최소한 3가지를 알아야한다.
-        // 1. sleep 사용시 다른 쓰레드에게 다른 리소스를 전달한다.
-            // 자는 동안 아무런 동작을 하지않기 때문
-        Thread threadLambda = new Thread(() -> {
-            try {
-                Thread.sleep(1000L);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("Lambda: " + Thread.currentThread().getName());
-        });
+        // graceful shutdown
+        // 현재 진행중인 작업은 끝마치고 끝내는 기능
+        executorService.shutdown();
+        //executorService.shutdownNow();
+        executorService = Executors.newFixedThreadPool(2);
+        executorService.submit(getRunnable("Hello"));
+        executorService.submit(getRunnable("9gin"));
+        executorService.submit(getRunnable("the"));
+        executorService.submit(getRunnable("java"));
+        executorService.submit(getRunnable("thread"));
+        executorService.shutdown();
 
-        // 2. interrupt 쓰레드를 깨우는 용도
-            // threadLambda(Thread) 안에 Thread.sleep() 사용해서 알아서 threadLambda(Thread)
-            // 를 인식하는 건가?
-            // 깨우는 용도지 종료 시키는 용도가 아니다.
-        Thread threadMethodRef = new Thread(App::run);
-        threadMethodRef.start();
-        try {
-            Thread.sleep(3000L);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        threadMethodRef.interrupt();
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        service.schedule(getRunnable("service = Hello: "), 5, TimeUnit.SECONDS);
 
-        // 3. join 다른(threadMethodRef) 쓰레드를 기다리는 기능
-        System.out.println("hello: " + Thread.currentThread().getName());
-        try {
-            threadMethodRef.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println(thread + " is finished");
-        // 쓰레드 끼리 계속 왔다갔다 하기 때문에 복잡해진다.? 난잡해진다.
+        service.shutdown();
+
+        ScheduledExecutorService service2 = Executors.newSingleThreadScheduledExecutor();
+        service2.scheduleAtFixedRate(getRunnable("service = fixed: "), 1, 2, TimeUnit.SECONDS);
 
     }
-    private static void run() {
-        while (true) {
-            System.out.println("MethodRef: " + Thread.currentThread().getName());
-            try {
-                Thread.sleep(1000L);
-            } catch (InterruptedException e) {
-                System.out.println("exit!");
-                return;
-            }
-        }
-    }
 
-    static class MyThread extends Thread{
-        @Override
-        public void run() {
-            System.out.println("Hello: " + Thread.currentThread().getName());
-        }
+    private static Runnable getRunnable(String message) {
+        return () -> System.out.println(message + " " + Thread.currentThread().getName());
     }
 }
